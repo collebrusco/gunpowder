f7bfa5e15400de612781e4e661482e5862f6388f
collebrusco <collebrusco.frank@gmail.com>
2024-01-03 00:41:23 -0600

    powder

--- src/pilot.cpp
+++ src/pilot.cpp
@@ -20,99 +20,193 @@ std::size_t operator()(const ivec2& obj) const {
 }};}
 
 struct Dot {
-
+	Dot(uint32_t u, uint32_t v, uint8_t r, uint8_t g, uint8_t b)
+	: x(u), y(v), color{r,g,b} {}
+	uint32_t x, y;
+	uint8_t color[3];
 };
 
 class DotField {
-	uint8_t* pixels;
+	uint8_t* _pixels;
 	uint32_t _x,_y, _size;
-	Texture tex;
 public:
+	ECS dots;
 	uint32_t x() {return _x;}
 	uint32_t y() {return _y;}
 	uint32_t size() {return _size;}
-	Texture const& texture() {return tex;}
+	uint8_t* pixels() {return _pixels;}
 	DotField(uint32_t x, uint32_t y) {
 		_x = x; _y = y;_size = x*y;
-		pixels = new uint8_t[3*_size];
+		_pixels = new uint8_t[3*_size];
 	}
-	void tinit() {
-		tex.create();
-		tex.bind();
-		tex.pixelate();
-    	tex.alloc(GL_TEXTURE_2D, 0, GL_RGB32F,
-                  _x, _y, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-    	tex.unbind();
+	~DotField(){delete [] _pixels;}
+	ivec2 mouse_pos() {
+		int x = static_cast<int>((window.mouse.pos.x / window.width) * this->x());
+		int y = static_cast<int>(((window.height - window.mouse.pos.y) / window.height) * this->y());
+		x = max(0,x); y = max(0,y);
+		return {x,y};
 	}
-	~DotField(){delete [] pixels;}
-	void color_texture(uint32_t i, uint8_t r, uint8_t g, uint8_t b) {
+	void color_texture(uint32_t i, uint8_t * clr) {
+		if (i < 0 || i > _size) {
+			LOG_ERR("accessing pixel buffer at %d, outside of %d size", i, _size); return;
+		}
 		i = 3*i;
-		pixels[i  ] = r;
-		pixels[i+1] = g;
-		pixels[i+2] = b;
+		memcpy(&_pixels[i], clr, 3);
 	}
-	void update_texture() {
-		tex.bind();
-    	tex.alloc(GL_TEXTURE_2D, 0, GL_RGB32F,
-                  _x, _y, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+	template<typename...Args>
+	void add_dot(uint32_t x, uint32_t y, Args... args) {
+		if (x < 0 || x > this->x() || y < 0 || y > this->y()) {
+			LOG_INF("add dot at (%d,%d) not allowed", x, y); return;
+		}
+		auto e = dots.newEntity();
+		dots.addComp<Dot>(e, x, y, args...);
+		lookup.set({x,y},e);
+	}
+	void update() {
+		for (auto e : dots.view<Dot>()) {
+			Dot& d = dots.getComp<Dot>(e);
+			if (((d.y * x()) + d.x) == -1) LOG_ERR("%d,%d = -1u!!!",d.x,d.y);
+			color_texture((d.y * x()) + d.x, d.color);
+		}
+	}
+
+	void clear() {
+		for (auto e : dots.view<Dot>()) {
+			Dot& d = dots.getComp<Dot>(e);
+			uint8_t z[] = {0x00,0x00,0x00,0x00};
+			if (((d.y * x()) + d.x) == -1) LOG_ERR("%d,%d = -1c!!!",d.x,d.y);
+			color_texture((d.y * x()) + d.x, z);
+		}
 	}
 
 	class DotLookup {
-		unordered_map<ivec2, Dot*> lookup;
+		unordered_map<ivec2, entID> lookup;
 	public:
-		void set(ivec2 pos, Dot * dot) {
+		void set(ivec2 pos, entID dot) {
 			lookup.insert({pos, dot});
 		}
 		bool empty(int x, int y) {
 			return lookup.end() == lookup.find({x, y});
 		}
-		Dot& get(int x, int y) {
-			return *lookup[{x,y}];
+		entID get(int x, int y) {
+			return lookup[{x,y}];
+		}
+		void clear() {
+			lookup.clear();
 		}
 	};
+	DotLookup lookup;
 };
 
-static DotField df(1280, 720);
+static DotField df(256, 256);
 
 struct Renderer {
 	Mesh<Vt_classic> quad;
 	Shader df_shad;
+	Texture df_tex;
 	Stopwatch timer;
 	Renderer() : timer(ftime::MILLISECONDS) {}
 	void init() {
 		quad = DefaultMeshes::tile<Vt_classic>();
 		df_shad = Shader::from_source("passthrough_vert", "tex");
+		df_tex.create();
+		df_tex.bind();
+		df_tex.pixelate();
+    	df_tex.alloc(GL_TEXTURE_2D, 0, GL_RGB32F,
+                  	df.x(), df.y(), GL_RGB, GL_UNSIGNED_BYTE, df.pixels());
+    	df_tex.unbind();
 	}
 	void render() {
 		gl.clear();
-		df.texture().bind();
+		df_tex.bind();
 
 		timer.reset_start();
-		df.update_texture();
+		df_tex.bind();
+    	df_tex.alloc(GL_TEXTURE_2D, 0, GL_RGB32F,
+                  	 df.x(), df.y(), GL_RGB, GL_UNSIGNED_BYTE, df.pixels());
 		double stop = timer.stop_reset();
-		LOG_DBG("updating took %3f ms", stop);
+		// LOG_DBG("updating took %3f ms", stop);
 
 		df_shad.bind();
 		timer.reset_start();
 		gl.draw_mesh(quad);
 		stop = timer.stop_reset();
-		LOG_DBG("drawing took %3f ms", stop);
+		// LOG_DBG("drawing took %3f ms", stop);
 	}
 };
 
 static Renderer renderer = Renderer();
 
+#include <stdlib.h>
+
 void Game::userCreate() {
 	renderer.init();
-	df.tinit();
-	for (int i = 0; i < df.size(); i++) {
-		df.color_texture(i, i&0x20 ? 0xFF : 0x00, 0x00, 0x00);
+	srand(656789);
+
+	for (int k = 0; k < 30; k++) {
+		for (int i = 0; i < 80; i++) {
+			int x = (128-40)+i;
+			int y = 128-k;
+			df.add_dot(x, y, 0xFC, 0xFC, 0xFC);
+			// auto e = df.dots.newEntity();
+			// df.dots.addComp<Dot>(e, x, y, 0xFC, 0xFC, 0xFC);
+			// df.lookup.set({x,y},e);
+		}
+	}
+
+
+	// for (int i = 0; i < df.size(); i++) {
+	// 	df.color_texture(i, i&0x20 ? 0xFF : 0x00, 0x00, 0x00);
+	// }
+}
+
+static void input() {
+	auto const& mouse = window.mouse;
+	if (mouse.left.down) {
+		ivec2 mpos = df.mouse_pos();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if ((mpos.x-5+i) < 0 || (mpos.y-5+j) < 0) continue;
+				if ((mpos.x-5+i) > df.x() || (mpos.y-5+j) > df.y()) continue;
+				if (df.lookup.empty(mpos.x-5+i,mpos.y-5+j))
+					df.add_dot(mpos.x-5+i,mpos.y-5+j,0xFE,0xFC,0xFF);
+			}
+		}
 	}
 }
 
 void Game::userUpdate(float dt) {
+	if (window.keyboard[GLFW_KEY_ESCAPE].pressed) this->close();
+	input();
+
+	df.clear();
+
+	for (auto e : df.dots.view<Dot>()) {
+		auto& d = df.dots.getComp<Dot>(e);
+		if (d.y > 0) {
+			if (df.lookup.empty(d.x,d.y-1)){
+				d.y--;
+			} else {
+				int32_t a = rand() & 0x00000002; a--;
+				if (d.x-a > 0 && d.x - a < df.x() && df.lookup.empty(d.x-a,d.y-1)) {
+					d.x-=a; d.y--;
+				} else if (d.x+a > 0 && d.x+a < df.x() && df.lookup.empty(d.x+a,d.y-1)) {
+					d.x+=a; d.y--;
+				}
+			}
+		}
+	}
+
+	df.lookup.clear();
+
+	for (auto e : df.dots.view<Dot>()) {
+		auto& d = df.dots.getComp<Dot>(e);
+		df.lookup.set({d.x,d.y},e);
+	}
+
+	df.update();
 	renderer.render();
-	LOG_INF("%2f FPS", 1./dt);
+	// LOG_INF("%2f FPS", 1./dt);
 }
 
 void Game::userDestroy() {
